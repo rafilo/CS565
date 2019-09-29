d499faf492abf70fbaf4a898df8aca94976eefb2
sherry wen <rikka9816@gmail.com>
2019-09-29 16:40:16 -0400

    merge all method in kmeans.py

--- project1/kmeans.py
+++ project1/kmeans.py
@@ -5,10 +5,27 @@ import random
 import pandas as pd
 import argparse
 import numpy as np 
+import matplotlib.pyplot as plt
+
+"""
+read a csv file according to its' path, have to manualy choose dimention
+"""
+def get_list_from_dataset_file(dataset_file):
+	dataset = pd.read_csv(dataset_file)
+	data = dataset[['revenue', 'vote_average', 'popularity']].astype(float).values.tolist()
+	idset = dataset['id']
+	return data, idset
+
+"""
+cost function, not implemented yer
+"""
+def cost_function(clustering):
+	tolerance = 0.001
+	pass
+
 
 """
 Accepts a list of points, each with the same number of dimensions.
-(points can have more dimensions than 2)  
 Returns a new point which is the center of all the points.
 """
 
@@ -26,7 +43,7 @@ def point_avg(points):
 Accepts a dataset and a list of assignments; the indexes 
 of both lists correspond to each other.
 Compute the center for each of the assigned groups.
-Return `k` centers in a list
+Return updated centers in a list
 """
 def update_centers(data_set, assignments):
 	centers = []
@@ -41,7 +58,9 @@ def update_centers(data_set, assignments):
 
 	return centers
 
-
+"""
+assign rest of points to its nearest cluster center
+"""
 def assign_points(data_points, centers):
     assignments = []
     for point in data_points:
@@ -56,7 +75,7 @@ def assign_points(data_points, centers):
     return assignments
 
 """
-Returns the Euclidean distance between a and b
+Returns the Euclidean distance between a and b (for dimention >= 2)
 """
 def distance(a, b):
 	squared_distance = 0
@@ -65,18 +84,19 @@ def distance(a, b):
 	return sqrt(squared_distance)
 
 """
-returns the absolute distance between a and b for 1-dimentional clustering
-"""
-def d1_distance(a, b):
-	return abs(a-b)
-
-
-"""
-Given `data_set`, which is an array of arrays,
 return a random set of k points from the data_set
 """
 def generate_k(data_set, k):
     return random.sample(population=data_set, k=k)
+
+# -------------------------------------------------------------
+# following function is for k++
+
+"""
+choose the next point with max proportional probability in k-means++
+"""
+def max_prop_probability(init_point, res_points):
+	pass
 
 """
 Given `data_set`, which is an array of arrays,
@@ -89,30 +109,58 @@ def generate_kpp(data_set, k):
 	while iteration > 0:
 		return 1
 
+#--------------------------------------------------------------
+# following function is for 1d kmeans
+"""
+returns the absolute distance between a and b for 1-dimentional clustering
+"""
+def d1_distance(a, b):
+	return abs(a-b)
 
-def get_list_from_dataset_file(dataset_file):
-	dataset = pd.read_csv(dataset_file)
-	data = dataset[['revenue', 'vote_average']].astype(float).values.tolist()
-	return data
 
-def cost_function(clustering):
-	pass
+#-------------------------------------------------------------
+# k_means main algorithm
+def k_means(dataset_file, k, init):
+	"""
+	1. generate k centers according to the rule (k-means, k-means++, 1d)
+	2. assign points to its nearest center (track old centers to determine the loss)
+	3. loop until centers don't change anymore (or use lost function)
+	4. output .csv file with movie's id and label
+	"""
+	dataset, idset = get_list_from_dataset_file(dataset_file)
 
-def k_means(dataset_file, k):
-	tolerance = 0.001
-	dataset = get_list_from_dataset_file(dataset_file)
-	k_points = generate_k(dataset, k)
+	if init == 'random':
+		k_points = generate_k(dataset, k)
+	elif init == 'k-means++':
+		# need to implement
+		pass
+	elif init == '1d':
+		# need to implement
+		pass
+
 	assignments = assign_points(dataset, k_points)
 	old_assignments = [0] * len(assignments)
+	final_centers = []
 
 	# need to add threshold
 	while assignments != old_assignments:
 		new_centers = update_centers(dataset, assignments)
 		old_assignments = assignments
 		assignments = assign_points(dataset, new_centers)
+		final_centers = new_centers
+
+	# assign group after threshold is met
 	clustering = defaultdict(list)
 	for assignment, point in zip(assignments, dataset):
 		clustering[assignment].append(point)
-	return clustering
+	
+	# outputs
+	final_df = pd.DataFrame({'id': idset, 'label': assignments})
+	final_df.to_csv('output.csv', index=False, header=True)
+	return final_df
 
-k_means('movie.csv', 3)
+parser = argparse.ArgumentParser()
+#parser.add_argument()
+k_means('./movie.csv', 5, 'random')
+
+
